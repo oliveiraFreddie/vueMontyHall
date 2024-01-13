4d15c96d9b6a25c7c0602ad0062db2e3b33e4fe2
oliveiraFreddie <freddie.oliveira95@gmail.com>
2024-01-13 12:04:31 +0000

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
   <div id="app">
     <h1>Problema de Monty Hall</h1>
-    <mhDoor :number="1" :hasGift="false" />
+    <div class="form">
+      <div v-if="!started">
+        <label for="portsAmount">Quantas portas</label>
+        <input
+          type="text"
+          id="portsAmount"
+          size="3"
+          v-model.number="portsAmount"
+        />
+      </div>
+      <div v-if="!started">
+        <label for="selectedPort">Qual a porta é premiada? </label>
+        <input
+          type="text"
+          id="selectedPort"
+          size="3"
+          v-model.number="selectedPort"
+        />
+      </div>
+      <button v-if="!started" @click="started = true">Iniciar</button>
+      <button v-if="started" @click="started = false">Reiniciar</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <mhDoor :hasGift="i === selectedPort" :number="i" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +36,14 @@ import mhDoor from './components/mhDoor.vue'
 
 export default {
   name: 'App',
-  components: { mhDoor }
+  components: { mhDoor },
+  data: function () {
+    return {
+      started: false,
+      portsAmount: 3,
+      seletedPort: null
+    }
+  }
 }
 </script>
 
@@ -36,5 +69,28 @@ body {
   background-color: #0004;
   padding: 20px;
   margin-bottom: 60px;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+
+.form,
+.form input,
+.form button {
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+
+.doors {
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+
+  flex-wrap: wrap;
 }
 </style>
